7aa132f7dee6197e7e20b78bc80d773f7eb45af0
Andrew Colannino <akcolannino@email.neit.edu>
2017-03-28 13:05:38 -0400

    import rollyball

---
+++ Assets/Scripts/Ball/GroundCheck_Grounded.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Placement:  Place on 'GroundCheck' gameobject
+
+// Purpose:  Tells the player if it is grounded or not, and modifies movement
+// 				based on slope angle.
+
+public class GroundCheck_Grounded : MonoBehaviour {
+
+	GameObject 
+		player;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		player = GameObject.FindWithTag ("Player");
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Ramp") {
+			player.GetComponent<BallController> ().IsJumping (false);
+		}
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+		if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Ramp") {
+			player.GetComponent<BallController> ().IsGrounded (true);
+		}
+
+		if(col.gameObject.tag == "Ramp"){
+			if (col.GetComponent<Terrain_ManageableTerrain>().ManageableTerrain == false) {
+				player.GetComponent<BallController> ().ToggleControls (false);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		player.GetComponent<BallController> ().ToggleControls (true);
+
+		if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Ramp") {
+			player.GetComponent<BallController> ().IsGrounded (false);
+		}
+	}
+}
